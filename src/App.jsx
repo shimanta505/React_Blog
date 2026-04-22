3dd93ae35d7103362fd41ac84050c54b4e6f0d89
shimanta505 <sarkershimanta@gmail.com>
2026-04-22 21:51:35 +0600

    react todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import {useState} from "react"
-import Weather from "./Weather"
+import TodoList from "./TodoList"
 
 const App = () => {
   
   return (
     <div>
       <h1>App Component</h1>
-      <Weather tempareture={26}></Weather>
+      <TodoList/>
     </div>
   );
 };
