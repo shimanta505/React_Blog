4754e0dd75e7e5e43b49a0754bfe9344790cab72
shimanta505 <sarkershimanta@gmail.com>
2026-04-28 19:36:11 +0600

    accordian

--- src/components/accordian/Index.jsx
+++ src/components/accordian/Index.jsx
@@ -6,24 +6,41 @@ import "./style.css";
 function Accordian() {
 
     const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multipleSelected, setMultiSelected] = useState([]);
 
     function handleSingleSelection(getCurrentId) {
         console.log(getCurrentId);
-        setSelected(getCurrentId === selected ? null: getCurrentId); 
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultiSelection(getCurrentItem) {
+        if (multipleSelected.includes(getCurrentItem)) {
+            let list = [...multipleSelected];
+            list.pop(getCurrentItem);
+            setMultiSelected(list);
+            return;
+        }
+
+        setMultiSelected([...multipleSelected, getCurrentItem]);
+        console.log(multipleSelected);
     }
 
     return (<div className="wrapper">
-
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)} >
+            Enable multi selection
+        </button>
         <div className="accordian">
             {
                 data && data.length > 0 ? (
                     data.map((dataItem) => (
                         <div className="item" key={dataItem.id}>
                             <div
-                                onClick={() => handleSingleSelection(dataItem.id)}
-                                className="title"
-                                
-                            >
+                                onClick=
+                                {enableMultiSelection ? () => handleMultiSelection(dataItem) :
+                                    () => handleSingleSelection(dataItem.id)
+                                }
+                                className="title">
                                 <h3>{dataItem.question}</h3>
                                 <span>+</span>
 
