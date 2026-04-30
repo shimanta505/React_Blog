585712aa89a35c01fb1191a676c657d64ec85d70
shimanta505 <sarkershimanta@gmail.com>
2026-04-30 22:17:18 +0600

    image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import RatingStar from "./components/rating-star";
+import ImageSlider from "./components/image-slider/ImageSlider";
 //import "./App.css";
 
 const App = () => {
 
   return (
     <div>
-      <RatingStar noOfStars={10}/>
+      <ImageSlider url="https://picsum.photos/v2/list" limit={5} page={1}/>
     </div>
   );
 };
