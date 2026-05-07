dc39bd74e8871f1a511ecf33306e6bae1522099f
shimanta505 <sarkershimanta@gmail.com>
2026-05-07 16:46:27 +0600

    custom hook

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -3,8 +3,15 @@ import "../components/index";
 
 function useCurrencyInfo(currency){
 
-    
+    const [data,setData] = useState({});
 
+    useEffect(()=> {
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`;
+        let response = fetch(url).then((res)=> res.json())
+        .then((res) => setData(res[currency]));
+    },[currency]);
+    console.log("curr-data",data);
+    return data;
 }
 
 export default useCurrencyInfo;
