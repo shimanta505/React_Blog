bbe436278816b09766b2c4aef5f697628b846483
shimanta505 <sarkershimanta@gmail.com>
2026-05-08 22:42:51 +0600

    start context api

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import "./index.css";
-import AppRouter from "./AppRouter.jsx";
-// import { createRoot } from "react-dom/client";
-// import "./index.css";
-// import AppRouter from "./AppRouter.js";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppRouter />
+    
   </StrictMode>,
 )
